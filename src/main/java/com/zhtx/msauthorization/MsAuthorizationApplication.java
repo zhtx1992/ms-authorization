c3451748287cf0f7f2b0e518969338f3d21bdb42
121649677 <121649677@qq.com>
2019-05-05 19:30:00 +0800

    new

---
+++ src/main/java/com/zhtx/msauthorization/MsAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.zhtx.msauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthorizationApplication.class, args);
+    }
+
+}
